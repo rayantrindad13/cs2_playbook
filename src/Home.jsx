74a7febfc099c4f1c73b6daba12e192228dd286b
rayan <rayanats.contato@gmail.com>
2025-05-21 15:57:39 -0300

    React HomeInit

---
+++ src/Home.jsx
@@ -0,0 +1,23 @@
+import imgHome from "./assets/cs-agg-playbook.jpg";
+import setaDir from "./assets/right-arrow.svg";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__img"
+          src={imgHome}
+          alt="Imagem CS Playbook Agressivo"
+        />
+
+        <Link to="/HomeMapa">
+          <img className="home__button" src={setaDir} alt="Seta Direita" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/HomeMapa.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeMapa = () => {
+  return <div>Escolha seu mapa</div>;
+};
+
+export default HomeMapa;
